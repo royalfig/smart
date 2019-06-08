77ca22367c60abb727fc06bc83ae2924ea998533
Ryan Feigenbaum <ryan.feigenbaum@gmail.com>
2019-06-08 18:15:02 -0400

    gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,46 @@
-'use strict';  const gulp = require('gulp'); const sass = require('gulp-sass');    sass.compiler = require('node-sass');  gulp.task('sass', function () {     return gulp.src('./src/sass/**/*.scss')         
-.pipe(sass().on('error', sass.logError))         .pipe(gulp.dest('./public/css')); });  gulp.task('watch', function () {     return gulp.watch('./src/sass/**/*.scss', gulp.series('sass')); });
+'use strict';
+const gulp = require('gulp');
+const sourcemaps = require('gulp-sourcemaps');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const clean = require('gulp-clean-css');
+
+// sass.compiler = require('node-sass');
+
+
+
+// gulp.task('sass', function () {
+//     return gulp.src('./src/sass/**/*.scss')
+//         .pipe(sass().on('error', sass.logError)).pipe(gulp.dest('./dist/css'));
+// });
+
+// CSS task
+function css() {
+    return gulp
+        .src("./src/scss/**/*.scss")
+        .pipe(sourcemaps.init())
+        // .pipe(plumber())
+        .pipe(sass({
+            outputStyle: "expanded"
+        }))
+        // .pipe(gulp.dest("./dist/css/"))
+        // .pipe(rename({
+        //     suffix: ".min"
+        // }))
+        //   .pipe(postcss([autoprefixer(), cssnano()]))
+        .pipe(autoprefixer({
+            cascade: false
+        }))
+        .pipe(clean())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest("./dist/css/"))
+    // .pipe(browsersync.stream());
+}
+
+const build = gulp.series(css);
+
+gulp.task('watch', function () {
+    return gulp.watch('./src/scss/**/*.scss', gulp.series('css'));
+});
+
+exports.css = css;
