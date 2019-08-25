68d33cb31c3c2f2312f395bdcc8b6f734da433df
Ryan Feigenbaum <ryan.feigenbaum@gmail.com>
2019-08-25 16:43:54 -0400

    svg

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,92 +1,86 @@
-// dEV 
+// dEV
 const dev = document.querySelector('.dev');
 
 function viewPort() {
-    if (window.innerWidth < 640) {
-        dev.textContent = `${window.innerWidth}px, PHONE`;
-    } else if (window.innerWidth > 639 && window.innerWidth < 1024) {
-        dev.textContent = `${window.innerWidth}px, TABLET`;
-    } else if (window.innerWidth > 1023 && window.innerWidth < 1280) {
-        dev.textContent = `${window.innerWidth}px, LAPTOP`;
-    } else {
-        dev.textContent = `${window.innerWidth}px, DESKTOP`;
-    }
+  if (window.innerWidth < 640) {
+    dev.textContent = `${window.innerWidth}px, PHONE`;
+  } else if (window.innerWidth > 639 && window.innerWidth < 1024) {
+    dev.textContent = `${window.innerWidth}px, TABLET`;
+  } else if (window.innerWidth > 1023 && window.innerWidth < 1280) {
+    dev.textContent = `${window.innerWidth}px, LAPTOP`;
+  } else {
+    dev.textContent = `${window.innerWidth}px, DESKTOP`;
+  }
 }
 
 viewPort();
 
 window.addEventListener('resize', viewPort);
 
-// Show and hide search bar 
+// Show and hide search bar
 const searchBoxBtn = document.getElementById('st-search-button');
 const searchBox = document.querySelector('.st-search-box');
 const cancelBtn = document.querySelector('.st-cancel');
 const searchBar = document.querySelector('.st-search-bar');
 
-searchBoxBtn.addEventListener('click', function () {
-    showSearchBox(transform)
-});
-cancelBtn.addEventListener('click', e => closeBox(e))
 
 function showSearchBox(cb) {
-    // searchBox.classList.add('st-search-box-max-height');
-    searchBox.style.display = "block";
-    let op = 0.1;
-    let timer = setInterval(() => {
-        if (op >= 1) {
-            console.log('op');
-            cb()
-            clearInterval(timer);
-        } else {
-            op += 0.05;
-            searchBox.style.opacity = op;
-        }
-    }, 10);
+  // searchBox.classList.add('st-search-box-max-height');
+  searchBox.style.display = 'block';
+  let op = 0.1;
+  const timer = setInterval(() => {
+    if (op >= 1) {
+      cb();
+      clearInterval(timer);
+    } else {
+      op += 0.05;
+      searchBox.style.opacity = op;
+    }
+  }, 10);
 
+  searchBar.firstElementChild.focus();
+  searchBox.addEventListener('click', (e) => {
+    const searchElements = [];
+    const searchBarChildren = searchBar.childNodes;
+    searchBarChildren.forEach((e) => searchElements.push(e));
+    searchElements.push(searchBar);
+    if (!searchElements.includes(e.target)) {
+      searchBox.style.display = 'none';
+      searchBox.style.opacity = '0';
+      searchBar.style.transform = 'translateY(-100%)';
+    }
+  });
 
-    // searchBox.style.display = "block";
-    // searchBox.style.opacity = "1";
-    searchBar.firstElementChild.focus();
-    searchBox.addEventListener('click', e => {
-        const searchElements = [];
-        const searchBarChildren = searchBar.childNodes;
-        searchBarChildren.forEach(e => searchElements.push(e))
-        searchElements.push(searchBar);
-        if (!searchElements.includes(e.target)) {
-            searchBox.style.display = "none";
-            searchBox.style.opacity = "0";
-            searchBar.style.transform = "translateY(-100%)"
-        }
-    })
-
-    searchBox.addEventListener('keyup', e => {
-        if (e.keyCode === 27) {
-            searchBox.style.display = "none";
-            searchBox.style.opacity = "0";
-            searchBar.style.transform = "translateY(-100%)"
-
-        };
-    })
+  searchBox.addEventListener('keyup', (e) => {
+    if (e.keyCode === 27) {
+      searchBox.style.display = 'none';
+      searchBox.style.opacity = '0';
+      searchBar.style.transform = 'translateY(-100%)';
+    }
+  });
 }
-
-
 
 function transform() {
-    console.log('trans');
-    searchBar.style.transform = "translateY(0%)";
+  searchBar.style.transform = 'translateY(0%)';
 }
 
-function closeBox(e) {
-    searchBox.style.display = "none";
-    searchBox.style.opacity = "0";
-    searchBar.style.transform = "translateY(-100%)";
+searchBoxBtn.addEventListener('click', () => {
+  showSearchBox(transform);
+});
+
+function closeBox() {
+  searchBox.style.display = 'none';
+  searchBox.style.opacity = '0';
+  searchBar.style.transform = 'translateY(-100%)';
 }
+
+cancelBtn.addEventListener('click', () => closeBox());
 
 // Add Grid styles
 const gridContainers = document.querySelectorAll('.st-grid-container-home');
-console.log(gridContainers);
-gridContainers.forEach(element => {
-    const gridItems = element.querySelectorAll('.st-home-post');
-    console.log(gridItems.length)
-    element.classList.add(`st-grid-container-home-${gridItems.length}`);
-})
+
+gridContainers.forEach((element) => {
+  const gridItems = element.querySelectorAll('.st-home-post');
+
+  element.classList.add(`st-grid-container-home-${gridItems.length}`);
+});
