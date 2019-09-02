06fe9346de18ea853356d1fbb3077a12334cb36a
Ryan Feigenbaum <ryan.feigenbaum@gmail.com>
2019-09-02 15:01:31 -0400

    styling

--- dist/js/main.js
+++ dist/js/main.js
@@ -17,65 +17,6 @@
 
 // window.addEventListener('resize', viewPort);
 
-// Show and hide search bar
-const searchBoxBtn = document.getElementById('search-button');
-const searchBox = document.querySelector('.search-box');
-const cancelBtn = document.querySelector('.cancel');
-const searchBar = document.querySelector('.search-bar');
-
-
-function showSearchBox(cb) {
-  // searchBox.classList.add('search-box-max-height');
-  searchBox.style.display = 'block';
-  let op = 0.1;
-  const timer = setInterval(() => {
-    if (op >= 1) {
-      cb();
-      clearInterval(timer);
-    } else {
-      op += 0.05;
-      searchBox.style.opacity = op;
-    }
-  }, 10);
-
-  searchBar.firstElementChild.focus();
-  searchBox.addEventListener('click', (e) => {
-    const searchElements = [];
-    const searchBarChildren = searchBar.childNodes;
-    searchBarChildren.forEach((e) => searchElements.push(e));
-    searchElements.push(searchBar);
-    if (!searchElements.includes(e.target)) {
-      searchBox.style.display = 'none';
-      searchBox.style.opacity = '0';
-      searchBar.style.transform = 'translateY(-100%)';
-    }
-  });
-
-  searchBox.addEventListener('keyup', (e) => {
-    if (e.keyCode === 27) {
-      searchBox.style.display = 'none';
-      searchBox.style.opacity = '0';
-      searchBar.style.transform = 'translateY(-100%)';
-    }
-  });
-}
-
-function transform() {
-  searchBar.style.transform = 'translateY(0%)';
-}
-
-searchBoxBtn.addEventListener('click', () => {
-  showSearchBox(transform);
-});
-
-function closeBox() {
-  searchBox.style.display = 'none';
-  searchBox.style.opacity = '0';
-  searchBar.style.transform = 'translateY(-100%)';
-}
-
-cancelBtn.addEventListener('click', () => closeBox());
-
 // Add Grid styles
 const gridContainers = document.querySelectorAll('.grid-container-home');
 
@@ -87,7 +28,7 @@ gridContainers.forEach((element) => {
 
 
 // Fly in fixed navbar
-// Will have to add padding to offset content
+// Will have to add padding to offset content?
 const fixedNavbar = document.querySelector('.fixed-navbar');
 const heroImage = document.querySelector('.hero-img');
 
@@ -95,10 +36,10 @@ let lastKnownScrollPos = 0;
 let ticking = false;
 
 function scroller(scrollPos) {
-  if (heroImage.offsetHeight < scrollPos) {
-    fixedNavbar.style.transform = 'translateY(0)';
+  if (heroImage.offsetHeight - (heroImage.offsetHeight * 0.9) < scrollPos) {
+    fixedNavbar.style.background = '#181818';
   } else {
-    fixedNavbar.style.transform = 'translateY(-100%)';
+    fixedNavbar.style.background = 'transparent';
   }
 }
 
