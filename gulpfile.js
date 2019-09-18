47fc643a4d2be39585b62fdfe89b46160ccc78d7
Ryan Feigenbaum <ryan.feigenbaum@gmail.com>
2019-09-18 00:25:59 -0400

    post tags

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,6 @@ const sourcemaps = require('gulp-sourcemaps');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const clean = require('gulp-clean-css');
-var evilIcons = require("gulp-evil-icons");
 
 // CSS task
 function css() {
@@ -24,9 +23,3 @@ function css() {
 gulp.task('watch', () => gulp.watch('./src/scss/**/*.scss', gulp.series('css')));
 
 exports.css = css;
-
-gulp.task('evil', function () {
-  return gulp.src('dist/home.html')
-    .pipe(evilIcons())
-    .pipe(gulp.dest('build'));
-});
