f338a5cc7c0e3a645a17b04e4f2c87f311cad890
Ryan Feigenbaum <ryan.feigenbaum@gmail.com>
2020-03-22 21:44:52 -0400

    updated webpack

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
+const postcssPresetEnv = require('postcss-preset-env');
+const cssnano = require('cssnano');
+
 module.exports = {
-  plugins: {
-    "postcss-preset-env": {},
-    cssnano: {}
-  }
+  plugins: [postcssPresetEnv(), cssnano()]
 };
