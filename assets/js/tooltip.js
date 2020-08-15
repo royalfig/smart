cacc7e8740dfdd1e9d7bf32c28cf1fdad31c2e67
Ryan Feigenbaum <ryan.feigenbaum@gmail.com>
2020-08-14 21:29:19 -0400

    updated styling

--- assets/js/tooltip.js
+++ assets/js/tooltip.js
@@ -16,37 +16,40 @@ const removeExistingTts = (el) => {
     el.removeChild(existingTt);
   }
 };
+const mouseoverHandler = (e) => {
+  const sourceEl = e.srcElement;
+  removeExistingTts(sourceEl);
+  const coords = e.srcElement.getBoundingClientRect();
+  const docWidth = document.documentElement.clientWidth;
+  const docHeight = document.documentElement.clientHeight;
+  const text = sourceEl.dataset.tt;
+
+  // Proximity check for right side
+  if (docWidth - coords.x < THRESHOLD) {
+    const tt = createHTML(text, 'left');
+    sourceEl.append(tt);
+    return;
+  }
+
+  // Proximity check for left side
+  if (coords.x < THRESHOLD) {
+    const tt = createHTML(text, 'right');
+    sourceEl.append(tt);
+    return;
+  }
+
+  // Proximity check for bottom side
+  if (docHeight - coords.y < THRESHOLD) {
+    const tt = createHTML(text, 'top');
+    sourceEl.append(tt);
+    return;
+  }
+
+  // Defaults to bottom
+  const tt = createHTML(text, 'bottom');
+  sourceEl.append(tt);
+};
+
 ttEl.forEach((el) => {
-  el.addEventListener('mouseover', (e) => {
-    removeExistingTts(el);
-    const coords = e.srcElement.getBoundingClientRect();
-    const docWidth = document.documentElement.clientWidth;
-    const docHeight = document.documentElement.clientHeight;
-    const text = el.dataset.tt;
-
-    // Proximity check for right side
-    if (docWidth - coords.x < THRESHOLD) {
-      const tt = createHTML(text, 'left');
-      el.append(tt);
-      return;
-    }
-
-    // Proximity check for left side
-    if (coords.x < THRESHOLD) {
-      const tt = createHTML(text, 'right');
-      el.append(tt);
-      return;
-    }
-
-    // Proximity check for bottom side
-    if (docHeight - coords.y < THRESHOLD) {
-      const tt = createHTML(text, 'top');
-      el.append(tt);
-      return;
-    }
-
-    // Defaults to bottom
-    const tt = createHTML(text, 'bottom');
-    el.append(tt);
-  });
+  el.addEventListener('mouseenter', mouseoverHandler.bind(this));
 });
