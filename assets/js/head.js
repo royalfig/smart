8e7cf7592591db0461fda6e392e6344cfc5499a7
Ryan Feigenbaum <ryan.feigenbaum@gmail.com>
2020-09-11 13:00:06 -0400

    updated css to use clamp with fallback

--- assets/js/head.js
+++ assets/js/head.js
@@ -16,47 +16,10 @@
   }
 })();
 
-((minVW, maxVW, minFontSize, maxFontSize) => {
-  const computeRem = () => {
-    const body = document.documentElement;
-    const properties = window.getComputedStyle(body);
-    const baseFontSize = properties.fontSize.replace(/px/, '');
-    // Gets the max font size of either the browser or the dev
-    const max = Math.max(minFontSize, baseFontSize);
-    const relativeMax = (maxFontSize * max) / minFontSize;
-    const maxRem = relativeMax / baseFontSize;
-    const minRem = max / baseFontSize;
-    return { maxRem, minRem };
-  };
-
-  const { maxRem } = computeRem();
-  const { minRem } = computeRem();
-
-  const fontSize = () => {
-    const width = window.innerWidth;
-    const height = window.outerHeight;
-    let rem = minRem;
-    const HEIGHT_THRESHOLD = 599;
-
-    if (width > minVW && width < maxVW && height > HEIGHT_THRESHOLD) {
-      rem = minRem + ((maxRem - minRem) * (width - minVW)) / (maxVW - minVW);
-    }
-
-    if (width > maxVW && height > HEIGHT_THRESHOLD) {
-      rem = maxRem;
-    }
-
-    document.documentElement.style = `font-size: ${rem}rem`;
-  };
-
-  const resizeHandler = () => {
-    fontSize();
-    window.addEventListener('resize', fontSize);
-  };
-
-  resizeHandler();
-})(640, 1280, 16, 20);
-
+/*
+* Set initial preference for color mode
+TODO Confirm behavior for unsupported browsers
+*/
 if (
   localStorage.getItem('pref') === 'light' ||
   (window.matchMedia('(prefers-color-scheme: light)').matches &&
