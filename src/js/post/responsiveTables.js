10f816ebee8ae3d6e2117012ca006986253bd5e7
Ryan Feigenbaum <ryan.feigenbaum@gmail.com>
2021-04-25 22:43:03 -0400

    updated styling

--- src/js/post/responsiveTables.js
+++ src/js/post/responsiveTables.js
@@ -8,7 +8,7 @@ export default function tablePrependInit() {
     const responsiveWrapper = document.createElement('div');
     responsiveWrapper.setAttribute(
       'style',
-      'width: 100%; overflow-x: auto; margin: 1em 0; border-radius: 4px; border: 1px solid var(--text4)',
+      'width: 100%; overflow-x: auto; margin: 2rem 0;',
     );
     e.parentNode.insertBefore(responsiveWrapper, e);
     responsiveWrapper.appendChild(e);
