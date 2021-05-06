8372098abb1a862ae9baf42cb3ada5397726e442
Ryan Feigenbaum <ryan.feigenbaum@gmail.com>
2021-05-05 22:31:05 -0400

    removed share

--- src/js/post/index.js
+++ src/js/post/index.js
@@ -2,12 +2,10 @@ import '../../scss/post.scss';
 
 import initMediumZoom from './medium_zoom';
 import fluidvids from './fluidvids';
-import shareProgressInit from './progress_and_share';
 import copyInit from './copy';
 import responsiveTableInit from './responsiveTables';
 
 initMediumZoom();
 fluidvids();
-shareProgressInit();
 copyInit();
 responsiveTableInit();
