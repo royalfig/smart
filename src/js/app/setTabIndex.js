fe8ecff6802f1a1b7b04add985f4a16b4111dac5
Ryan Feigenbaum <ryan.feigenbaum@gmail.com>
2021-06-16 21:11:33 -0400

    fixed reference to id for tab index

--- src/js/app/setTabIndex.js
+++ src/js/app/setTabIndex.js
@@ -1,4 +1,4 @@
 export default function removeNavLinks() {
-  const menuNavLinks = document.querySelectorAll('#menu a');
+  const menuNavLinks = document.querySelectorAll('#sm-menu a');
   menuNavLinks.forEach((el) => el.setAttribute('tabindex', '-1'));
 }
