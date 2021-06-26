33dde08ab9a5558b0cc5716de666cd2a2590635d
Ryan Feigenbaum <ryan.feigenbaum@gmail.com>
2021-06-26 19:54:11 -0400

    image selection

--- src/js/post/medium_zoom.js
+++ src/js/post/medium_zoom.js
@@ -3,9 +3,17 @@ import mediumZoom from 'medium-zoom';
 // Medium Zoom
 export default function initMediumZoom() {
   const directImages = document.querySelectorAll('.sm-post__content > img');
+  const markdownImages = document.querySelectorAll(
+    '.sm-post__content > p > img',
+  );
   const kgImages = document.querySelectorAll('.kg-image-card > img');
   const galleryImages = document.querySelectorAll('.kg-gallery-image > img');
-  const postImages = [...directImages, ...kgImages, ...galleryImages];
+  const postImages = [
+    ...directImages,
+    ...markdownImages,
+    ...kgImages,
+    ...galleryImages,
+  ];
 
   mediumZoom(postImages, {
     background: 'rgba(0,0,0,0.75)',
