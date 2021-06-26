dc254696e016cd4c113990a4677c427041a6ec9f
Ryan Feigenbaum <ryan.feigenbaum@gmail.com>
2021-06-26 19:51:44 -0400

    class on img zoom

--- src/js/post/medium_zoom.js
+++ src/js/post/medium_zoom.js
@@ -2,7 +2,7 @@ import mediumZoom from 'medium-zoom';
 
 // Medium Zoom
 export default function initMediumZoom() {
-  const directImages = document.querySelectorAll('.sm-post > img');
+  const directImages = document.querySelectorAll('.sm-post__content > img');
   const kgImages = document.querySelectorAll('.kg-image-card > img');
   const galleryImages = document.querySelectorAll('.kg-gallery-image > img');
   const postImages = [...directImages, ...kgImages, ...galleryImages];
