4ce03d24379d0a3dda2e7b14e9265ee3a7f02651
Ryan Feigenbaum <ryan.feigenbaum@gmail.com>
2022-12-20 10:45:24 -0500

    wip

--- assets/built/generateColorPalette.js
+++ assets/built/generateColorPalette.js
@@ -1,2 +1,71 @@
-!function(){"use strict";function t(t){let e=0,n=0,o=0;4===t.length?(e=`0x${t[1]}${t[1]}`,n=`0x${t[2]}${t[2]}`,o=`0x${t[3]}${t[3]}`):7===t.length&&(e=`0x${t[1]}${t[2]}`,n=`0x${t[3]}${t[4]}`,o=`0x${t[5]}${t[6]}`),e/=255,n/=255,o/=255;const c=Math.min(e,n,o),m=Math.max(e,n,o),l=m-c;let r=0,u=0,s=0;return r=0===l?0:m===e?(n-o)/l%6:m===n?(o-e)/l+2:(e-n)/l+4,r=Math.round(60*r),r<0&&(r+=360),s=(m+c)/2,u=0===l?0:l/(1-Math.abs(2*s-1)),u=+(100*u).toFixed(1),s=+(100*s).toFixed(1),[r,u,s]}!function(){const[e,n,o]=document.documentElement.dataset.accentColor?t(document.documentElement.dataset.accentColor):t("#ff0000"),c=e+180>360?e-180:e+180;document.documentElement.style.setProperty("--primary-h",e),document.documentElement.style.setProperty("--saturation",`${n}%`),document.documentElement.style.setProperty("--lightness",`${o}%`),document.documentElement.style.setProperty("--complementary-color",c)}()}();
+(function () {
+  'use strict';
+
+  // From CSS Tricks
+  function hexToHSL(H) {
+    // Convert hex to RGB first
+    let r = 0;
+    let g = 0;
+    let b = 0;
+    if (H.length === 4) {
+      r = `0x${H[1]}${H[1]}`;
+      g = `0x${H[2]}${H[2]}`;
+      b = `0x${H[3]}${H[3]}`;
+    } else if (H.length === 7) {
+      r = `0x${H[1]}${H[2]}`;
+      g = `0x${H[3]}${H[4]}`;
+      b = `0x${H[5]}${H[6]}`;
+    }
+    // Then to HSL
+    r /= 255;
+    g /= 255;
+    b /= 255;
+    const cmin = Math.min(r, g, b);
+    const cmax = Math.max(r, g, b);
+    const delta = cmax - cmin;
+    let h = 0;
+    let s = 0;
+    let l = 0;
+
+    if (delta === 0) {
+      h = 0;
+    } else if (cmax === r) {
+      h = ((g - b) / delta) % 6;
+    } else if (cmax === g) {
+      h = (b - r) / delta + 2;
+    } else {
+      h = (r - g) / delta + 4;
+    }
+
+    h = Math.round(h * 60);
+
+    if (h < 0) {
+      h += 360;
+    }
+
+    l = (cmax + cmin) / 2;
+    s = delta === 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+    s = +(s * 100).toFixed(1);
+    l = +(l * 100).toFixed(1);
+
+    return [h, s, l];
+  }
+
+  function generateColorPalette() {
+    const [h, s, l] = document.documentElement.dataset.accentColor
+      ? hexToHSL(document.documentElement.dataset.accentColor)
+      : hexToHSL('#ff0000');
+    const complementaryColor = h + 180 > 360 ? h - 180 : h + 180;
+    document.documentElement.style.setProperty('--primary-h', h);
+    document.documentElement.style.setProperty('--saturation', `${s}%`);
+    document.documentElement.style.setProperty('--lightness', `${l}%`);
+    document.documentElement.style.setProperty(
+      '--complementary-color',
+      complementaryColor,
+    );
+  }
+
+  generateColorPalette();
+
+})();
 //# sourceMappingURL=generateColorPalette.js.map
