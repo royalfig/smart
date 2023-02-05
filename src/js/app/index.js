6cf86181fdb1256bf0e228b2e958ab6de0ec0606
Ryan Feigenbaum <ryan.feigenbaum@gmail.com>
2023-02-05 10:33:26 -0500

    add navbar animation

--- src/js/app/index.js
+++ src/js/app/index.js
@@ -1,6 +1,7 @@
 import '../../css/app/index.css';
 
 import initClickHandler from './clickHandler';
+import animateNavbar from './navbarAnimation';
 import { determineColorModeSupport } from './colorModeToggle';
 import keyClickHandler from './keyHandler';
 import generatePagination from './pagination';
@@ -17,6 +18,7 @@ if (ENV === 'development') {
 }
 
 initClickHandler();
+animateNavbar();
 determineColorModeSupport();
 generatePagination();
 keyClickHandler();
---
+++ src/js/app/navbarAnimation.js
@@ -0,0 +1,17 @@
+export default function animateNavbar() {
+  const height = document.documentElement.clientHeight;
+  const scrollHeight = document.documentElement.scrollHeight;
+  const navbar = document.querySelector('.sm-navbar-container');
+  navbar.style.transition = 'background-color var(--transition)';
+
+  document.addEventListener('scroll', () => {
+    const scrollPercentage =
+      Math.round(window.scrollY) / (scrollHeight - height);
+
+    if (scrollPercentage > 0.15) {
+      navbar.style.backgroundColor = `var(--surface-light)`;
+    } else {
+      navbar.style.backgroundColor = `var(--surface)`;
+    }
+  });
+}
