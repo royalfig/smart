f100ff638e4453ccc57bec579b7f8be801112bd2
Ryan Feigenbaum <ryan.feigenbaum@gmail.com>
2023-04-26 19:00:23 -0400

    dark mode and authors

--- assets/js/app/colorModeToggle.js
+++ assets/js/app/colorModeToggle.js
@@ -3,12 +3,21 @@
  */
 
 export function toggleColorMode(color) {
+  const commentsIframe = document.querySelector('iframe[title="comments-box"]');
+
+  // Reload comments iframe when toggling color mode
+  if (commentsIframe) {
+    commentsIframe.contentWindow.location.reload();
+  }
+
   document.documentElement.setAttribute('data-color-pref', color);
   localStorage.setItem('pref', color);
 }
 
 export function determineColorModeSupport() {
-  const colorPrefButtons = document.querySelectorAll('.sm-dark-mode, .sm-light-mode');
+  const colorPrefButtons = document.querySelectorAll(
+    '.sm-dark-mode, .sm-light-mode',
+  );
 
   const hasSupport = window.CSS && CSS.supports('color', 'var(--primary)');
 
