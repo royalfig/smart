96882a1803141f8baebde4926a19c2c7611eca3d
Ryan Feigenbaum <ryan.feigenbaum@gmail.com>
2023-07-14 09:42:36 -0400

    again

--- assets/js/app/clickHandler.js
+++ assets/js/app/clickHandler.js
@@ -1,24 +1,18 @@
 import { toggleColorMode } from './colorModeToggle';
 import copyToClipboard from './copy';
-import { save } from './saves';
 
 function handleClick(e) {
   if (e.target.closest('.sm-navbar-menu-button')) {
-    document
-      .querySelector('.sm-nav-menu-container')
-      .classList.add('sm-show-menu');
-  }
+    const navbarBtn = document.querySelector('.sm-navbar-menu-button');
+    const navMenu = document.querySelector('.sm-nav-menu');
+    navMenu.classList.toggle('sm-show-nav-menu');
+    if (navbarBtn.getAttribute('aria-expanded') === 'false') {
+      navbarBtn.setAttribute('aria-expanded', 'true');
+      return;
+    }
+    navbarBtn.setAttribute('aria-expanded', 'false');
 
-  if (e.target.closest('.sm-love-button')) {
-    save(e.target.closest('.sm-love-button'));
-    // e.target.closest('.sm-love-button').classList.toggle('sm-love-toggle');
     return;
-  }
-
-  if (e.target.closest('.sm-nav-menu-close-button')) {
-    document
-      .querySelector('.sm-nav-menu-container')
-      .classList.remove('sm-show-menu');
   }
 
   if (e.target.closest('.sm-copy-button')) {
@@ -33,16 +27,6 @@ function handleClick(e) {
 
   if (e.target.closest('.sm-light-mode')) {
     toggleColorMode('light');
-    return;
-  }
-
-  if (
-    e.target.closest('.sm-saves-button') ||
-    e.target.closest('.sm-navbar-close-saves')
-  ) {
-    document
-      .querySelector('.sm-navbar-saves')
-      .classList.toggle('sm-show-saves');
   }
 }
 
