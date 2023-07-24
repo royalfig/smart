85841a3e339f14ea6e19d4146f968e13e528a971
Ryan Feigenbaum <ryan.feigenbaum@gmail.com>
2023-07-23 20:54:17 -0400

    share

--- assets/js/app/toggleShareMenu.js
+++ assets/js/app/toggleShareMenu.js
@@ -4,8 +4,21 @@ export default function toggleShareMenu() {
   const shareMenu = document.querySelector('.sm-share-menu');
   const closeButton = document.querySelector('.sm-share-close');
 
-  shareButton.addEventListener('click', () => {
-    shareMenu.showModal();
+  shareButton.addEventListener('click', (e) => {
+    if (document.documentElement.clientWidth < 500 && navigator.share) {
+      const { title, url } = e.target.dataset;
+
+      try {
+        navigator.share({
+          title,
+          url,
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    } else {
+      shareMenu.showModal();
+    }
   });
 
   closeButton.addEventListener('click', () => {
